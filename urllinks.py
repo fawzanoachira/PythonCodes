1804a4180bcd80500d57c24c64fd1517d2b36961
fawzanicfoss <fawzanicfoss@gmail.com>
2024-04-03 12:57:40 +0530

    .

---
+++ urllinks.py
@@ -0,0 +1,26 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+pos = int(input()) - 1
+count_limit = int(input())
+count = 0
+
+while(count < count_limit):
+
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Retrieve all of the anchor tags
+    tags = soup('a')
+    url = tags[pos].get('href')
+    name = tags[pos].contents[0]
+    count += 1
+
+print(name)
